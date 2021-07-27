a0464d26046cbd5fb0f16b482d52393855527655
Kejoh <kejoh07@gmail.com>
2021-07-27 16:24:02 +0200

    book by react

---
+++ mern-app/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51K6iSgqUqL._SX329_BO1,204,203,200_.jpg" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
